2f6ee236a7a1de30cf771e1b3d6d5976fa2db11c
tokujiTO <tiagomassuda123@gmail.com>
2025-10-06 15:39:22 -0300

    feat: add tailwindcss, react-router-dom and initial pages to the project

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-black hover:scale-110 hover:-translate-y-1.5 duration-300 hover:cursor-pointer">
+        This is the home screen with Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
