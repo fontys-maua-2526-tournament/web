9060591b4007842e0007c33a3f03728caacd9d62
JansenBhodi <117980774+JansenBhodi@users.noreply.github.com>
2025-10-30 11:45:46 +0100

    tailwind implementation
    Finished prettifying of view to be in line with the figma model for the most part.

--- src/pages/teamView.jsx
+++ src/pages/teamView.jsx
@@ -1,8 +1,8 @@
-import Navbar from '../components/navbar';
 import CustomButton from '../components/customButton';
-import CustomTextField from '../components/customTextField';
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { PlusCircleIcon } from '@phosphor-icons/react';
 
 const TeamViewer = () => {
     const [teams, setTeams] = useState([]);
@@ -35,27 +35,22 @@ const TeamViewer = () => {
 
 
     return (
-        <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>team Id</th>
-                        <th>Team Name</th>
-                    </tr>
-                </tbody>
+        <div className="flex flex-col flex-1 p-8 ml-20">
+            <div className="flex items-center gap-4 mb-8">
+                <h1 className="text-4xl font-bold text-gray-900">My Teams</h1>
+                <Link to={`/teams/new`}>
+                    <PlusCircleIcon className='text-purple-600 hover:text-purple-800 transition-colors'/>
+                </Link>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                 {teams.map(team => (
-                   <tbody>
-                        <tr>
-                            <td>
-                                {team.id}
-                            </td>
-                            <td>
-                                {team.name}
-                            </td>
-                        </tr>
-                   </tbody> 
+                    <Link className='flex items-end justify-center h-48 bg-white border-2 border-black rounded-xl shadow-md hover:shadow-lg transition-all duration-200' to={`/teams/${team.id}`} >
+                        <div className='w-full bg-white rounded-b-xl py-3 text-center font-semibold text-purple-600'>
+                            {team.name}
+                        </div>
+                    </Link>
                 ))}
-            </table>
+            </div>
         </div>
     );
 };
