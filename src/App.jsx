907dac5b866eee5c20be3e4d25fdec81eecdefbb
JansenBhodi <117980774+JansenBhodi@users.noreply.github.com>
2025-10-30 10:26:53 +0100

    Navbar implementation
    Navbar now globally appears in the app and does not need to be added separately to each page. Also please use link for navigating

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
-import CreateTournament from "./pages/CreateTournament";
-import { TeamProvider } from "@/app/context/team-context";
-import { TournamentProvider } from "@/app/context/tournament-context";
+import TeamViewer from "./pages/teamView";
+import Navbar from "./components/navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/tournaments" element={<CreateTournament />} />
-      </Routes>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/teamview" element={<TeamViewer />} />
+        </Routes>
+      </div>
+    </div>
     </BrowserRouter>
   );
 }
