e31cbd36b79334ec7bc478c98b67d5e8c9e9a1fd
Francisco Sanchez <i501613@fontys.student.nl>
2025-10-30 12:22:17 +0100

    Removed import for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
-import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 
 export default function App() {
