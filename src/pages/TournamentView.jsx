c3945aa91726844ff7af0bba92cadaf643134dfc
Francisco Sanchez <i501613@fontys.student.nl>
2025-11-03 10:50:19 +0100

    Redesigned list of tournaments

--- src/pages/TournamentView.jsx
+++ src/pages/TournamentView.jsx
@@ -34,21 +34,38 @@ function TournamentView() {
 
 
     return (
-        <div className="flex flex-col flex-1 p-8 ml-20">
-            <div className="flex items-center gap-4 mb-8">
-                <h1 className="text-4xl font-bold text-gray-900">Tournaments</h1>
-                <Link to={`/tournaments/create`}>
-                    <LucidePlusCircle className='text-purple-600 hover:text-purple-800 transition-colors'/>
+        <div className="flex flex-col flex-1 p-8 m-10">
+            <div className="flex items-center justify-between mb-8">
+                <h1 className="text-4xl font-bold text-gray-900">My Tournaments</h1>
+
+                <Link
+                to="/tournaments/create"
+                className="inline-flex items-center gap-2 bg-fontyssPurple text-white px-4 py-2 rounded-xl shadow hover:bg-[#874c95] transition-colors">
+                    <LucidePlusCircle className="w-5 h-5" />
+                    Add Tournament
                 </Link>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                {tournaments.map(tournament => (
-                    <Link className='flex items-end justify-center h-48 bg-white border-2 border-black rounded-xl shadow-md hover:shadow-lg transition-all duration-200' to={`/teams/${team.id}`} >
-                        <div className='w-full bg-white rounded-b-xl py-3 text-center font-semibold text-purple-600'>
-                            {tournament.name}
-                        </div>
-                    </Link>
-                ))}
+
+            <div className="flex flex-col gap-4">
+            {tournaments.map((tournament) => (
+                <Link
+                key={tournament.id}
+                to={`/tournaments/${tournament.id}`}
+                className="flex items-center h-20 rounded-xl overflow-hidden shadow-md 
+                            bg-[#BDADC0] hover:bg-[#9A8FA0] hover:-translate-y-1 
+                            hover:shadow-xl transition-all duration-200"
+                >
+                {/* Smaller dark purple section */}
+                <div className="w-6 h-full bg-fontyssPurple" />
+
+                {/* Lighter purple section with text */}
+                <div className="flex-1 h-full flex items-center px-6">
+                    <span className="text-lg font-semibold text-black transition-colors duration-200">
+                    {tournament.name}
+                    </span>
+                </div>
+                </Link>
+            ))}
             </div>
         </div>
     );
