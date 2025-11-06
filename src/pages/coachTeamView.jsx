b8d384b051f27ae0fe27150c44d4dcea0c1a6cfc
yasmin bonilha <bonilhayaya@gmail.com>
2025-11-06 19:55:57 -0300

    feat: coachTeamView page

--- src/pages/coachTeamView.jsx
+++ src/pages/coachTeamView.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { LucidePlusCircle } from 'lucide-react';
 
 export default function CoachTeamView(){
+
+    const [teams, setTeams] = useState([]);
+    
      
 
     return(
-        <div className="flex flex-col"> 
-            <div className="flex items-center">
-                <h1 className="text-black text-4xl font-bold duration-300 hover:-translate-y-1.5 hover:scale-101 mb-8">
+        <div className="flex flex-col flex-1 p-8 ml-20"> 
+            <div className="flex items-center gap-4 mb-8">
+                <h1 className="text-black text-4xl font-bold duration-300 hover:-translate-y-1.5 hover:scale-101">
                     My Teams
                 </h1>
+                <Link to={`/teams/new`}>
+                    <LucidePlusCircle className='text-purple-600 hover:text-fontyssPurple transition-colors'/>
+                </Link>
+            </div>
+            <div>
+                
             </div>
         </div>
     )
