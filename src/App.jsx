835f257d94c9b918bad6faaf4c5008edbb779e4a
tokujiTO <tiagomassuda123@gmail.com>
2025-12-02 15:13:40 -0300

    feat: add user context to the files of the project

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,41 @@ import TeamViewer from './pages/teamView';
 import TeamCreate from './pages/teamCreate';
 import TournamentView from './pages/TournamentView';
 import TournamentDetails from './pages/TournamentDetails';
+import { UserProvider } from './context/userContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="flex h-screen">
-        <Sidebar />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          limit={2}
-          theme="colored"
-        />
-        <div className="flex-1 overflow-y-auto bg-gray-50 p-5">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/teams/view" element={<TeamViewer />} />
-            <Route path="/teams/new" element={<TeamCreate />} />
-            <Route path="/tournaments" element={<TournamentView />} />
-            <Route path="/tournaments/:id" element={<TournamentDetails />} />
-          </Routes>
+      <UserProvider>
+        <div className="flex h-screen">
+          <Sidebar />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            limit={2}
+            theme="colored"
+          />
+          <div className="flex-1 overflow-y-auto bg-gray-50 p-5">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/teams/view" element={<TeamViewer />} />
+              <Route path="/teams/new" element={<TeamCreate />} />
+              <Route path="/tournaments" element={<TournamentView />} />
+              <Route path="/tournaments/:id" element={<TournamentDetails />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </UserProvider>
     </BrowserRouter>
   );
 }
