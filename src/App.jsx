f16f9a5209346804c224b855fa49aaa56faf35c8
tokujiTO <tiagomassuda123@gmail.com>
2025-12-09 14:58:26 -0300

    fix: app.jsx import error

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import TournamentView from './pages/TournamentView';
 import TeamCreate from './pages/teamCreate';
 import TournamentDetails from './pages/TournamentDetails';
 import { UserProvider } from './context/userContext';
-import TournamentCreate from './pages/TournamentCreate';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -23,37 +22,37 @@ export default function App() {
 
   return (
     <div className="flex h-screen">
-    <UserProvider>
-      <Sidebar hideIcons={isLoginPage} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        limit={2}
-        theme="colored"
-      />
-      <div className="flex-1 overflow-y-auto bg-gray-50 p-5">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+      <UserProvider>
+        <Sidebar hideIcons={isLoginPage} />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          limit={2}
+          theme="colored"
+        />
+        <div className="flex-1 overflow-y-auto bg-gray-50 p-5">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-          <Route path="/teams/view" element={<TeamViewer />} />
-          <Route path="/teams/new" element={<TeamCreate />} />
-          <Route path="/teams/:teamId" element={<TeamDetail />} />
-          <Route path="/tournaments" element={<TournamentView />} />
-          <Route path="/tournaments/:id" element={<TournamentDetails />} />
-          <Route path="/tournaments/create" element={<TournamentCreate />} />
-          <Route path="/user/register" element={<RegisterUser />} />
-          <Route path="/coach/teams/view" element={<CoachTeamView />} />
-        </Routes>
-      </div>
-    </UserProvider>
+            <Route path="/teams/view" element={<TeamViewer />} />
+            <Route path="/teams/new" element={<TeamCreate />} />
+            <Route path="/teams/:teamId" element={<TeamDetail />} />
+            <Route path="/tournaments" element={<TournamentView />} />
+            <Route path="/tournaments/:id" element={<TournamentDetails />} />
+            <Route path="/tournaments/create" element={<TournamentCreate />} />
+            <Route path="/user/register" element={<RegisterUser />} />
+            <Route path="/coach/teams/view" element={<CoachTeamView />} />
+          </Routes>
+        </div>
+      </UserProvider>
     </div>
   );
 }
